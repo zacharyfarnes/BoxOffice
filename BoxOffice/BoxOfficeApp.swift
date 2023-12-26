0452b4b9589e4ad1e1e2fc9ab56536282d7d4a75
Zachary Farnes <zachary.farnes@gmail.com>
2023-12-26 10:34:31 +0000

    Initial Commit

---
+++ BoxOffice/BoxOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeApp.swift
+//  BoxOffice
+//
+//  Created by Zachary Farnes on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoxOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
